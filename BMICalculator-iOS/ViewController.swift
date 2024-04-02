30ac7d6dfbac44c08bae4fe1b61af0245778492b
Mansour <theboom.kajas@gmail.com>
2024-04-02 12:34:08 +0200

    Initial Commit

---
+++ BMICalculator-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator-iOS
+//
+//  Created by Mañanas on 2/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
